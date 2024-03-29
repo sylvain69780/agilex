79e63e40ed21fb8f36c58a56a18ba1d06b29c429
LE COURTOIS Sylvain <sylvain.lecourtois@fr.adecco.net>
2024-03-30 00:36:59 +0100

    k,

---
+++ Agilex/Agilex/AppSettings.cs
@@ -0,0 +1,15 @@
+﻿namespace Agilex
+{
+    public class AgileValue
+    {
+        public string? Value { get; set; }
+        public string? WordToGuess { get; set; }
+        public string? Phrase { get; set; }
+        public string? InsteadOf { get; set; }
+    }
+    public class AppSettings
+    {
+        public AgileValue[]? AgileValues { get; set; }
+    }
+
+}
